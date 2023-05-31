aa00a027c958f6abe71d1dabad940b1700391876
Banyar <nainghtaw@gmail.com>
2023-05-31 17:29:38 +0800

    init commit

---
+++ MyCV/MyCV/ContentView.swift
@@ -0,0 +1,31 @@
+//
+/**
+  ContentView.swift
+
+  🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻
+  
+  Created by: Banyar on 5/31/23.
+  Copyright (c) 2023 Appzgate Solutions.
+*/
+//
+  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("My CV")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCV/MyCV/MyCVApp.swift
@@ -0,0 +1,22 @@
+//
+/**
+  MyCVApp.swift
+
+  🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻 🧑‍💻
+  
+  Created by: Banyar on 5/31/23.
+  Copyright (c) 2023 Appzgate Solutions.
+*/
+//
+  
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
